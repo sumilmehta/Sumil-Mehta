f6abc2f5b0ece1f7038dcb3b18c7bd24ca8a1499
sumilmehta <sumilmehta007@gmail.com>
2017-05-04 18:23:46 +0530

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 
-$to = 'sumilmehta007@gmail.com'
+$to = 'contact@sumilmehta.space'
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@sumilmehta.space\n"; 
